2bd5cd550d282475adff49ea6689eb6c0005f66a
Denisse Figueroa <denisse.figueroa@automatadg.com>
2021-09-09 14:08:12 -0600

    Analizador FCA
    Léxico y Sintáctico creado

---
+++ Analizador_ejempplo1/Analizador/src/generadores/GLexicoFCA.java
@@ -0,0 +1,26 @@
+
+import java.io.File;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 50235
+ */
+public class GLexicoFCA {
+    public static void main(String[] args) 
+    {
+        String path="src/analizadores/A_lexicoFCA.jflex";
+        generarLexer(path);
+    } 
+    
+    public static void generarLexer(String path)
+    {
+        File file=new File(path);
+        jflex.Main.generate(file);
+    } 
+}
